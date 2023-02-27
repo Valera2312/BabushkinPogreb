a12e9f917c7d48d9230bbd0db22e048c57b934a7
123 <79242258075@yandex.ru>
2023-02-28 09:42:00 +1000

    бот

---
+++ dispatcher/src/main/java/ru/proskuryakov/controller/TelegramBot.java
@@ -0,0 +1,30 @@
+package ru.proskuryakov.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    @Value("${bot.name}")
+    String botName;
+    @Value("${bot.token}")
+    String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originalMessage = update.getMessage();
+        System.out.println(originalMessage.getText());
+    }
+}
