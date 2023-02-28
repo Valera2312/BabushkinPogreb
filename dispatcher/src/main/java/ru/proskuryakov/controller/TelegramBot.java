ea6a819ea9d8ce0a34ed9f2e868f169cc42b257b
123 <79242258075@yandex.ru>
2023-02-28 10:09:01 +1000

    урок 1

--- dispatcher/src/main/java/ru/proskuryakov/controller/TelegramBot.java
+++ dispatcher/src/main/java/ru/proskuryakov/controller/TelegramBot.java
@@ -1,11 +1,15 @@
 package ru.proskuryakov.controller;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
+@Log4j
 public class TelegramBot extends TelegramLongPollingBot {
     @Value("${bot.name}")
     String botName;
@@ -25,6 +29,19 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         var originalMessage = update.getMessage();
-        System.out.println(originalMessage.getText());
+        log.debug(originalMessage.getText());
+        var response  = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Hello from bot");
+        sendAnswerMessage(response);
+    }
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
     }
 }
